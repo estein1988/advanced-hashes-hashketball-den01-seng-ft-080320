b5d7d1eb5676b2acfb5f733ffe64adb4da558e86
Elliott Stein <estein1988@gmail.com>
2020-07-24 15:03:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -177,7 +177,8 @@ end
 def player_stats(player_name)
   game_hash.each do |location, team_data|
     team_data[:players].each do |player|
-      if 
+      if player[:player_name] == player_name
+        return 
   binding.pry
     end 
   end 
