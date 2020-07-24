8e2a436c27561c09715e3ef02110a29402c374bf
Elliott Stein <estein1988@gmail.com>
2020-07-24 14:48:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -168,7 +168,7 @@ def player_numbers(team_name)
   game_hash.each do |location, team_data|
     if team_data[:team_name] == team_name 
       return team_data[:players].collect do |jersey_number|
-        players[:number]
+        [:number].value
       end 
     end
   end 
